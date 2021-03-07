46331c8783b3a7cdebdf91f94b803a4237f2f4fe
rg <rgenius@yandex.ru>
2021-03-07 16:39:22 +0300

    Change resolution parameter from tuple (int, int) to int (index)

--- screen.py
+++ screen.py
@@ -5,8 +5,8 @@ from pygame.display import set_mode
 class Screen:
     resolution_list = ((800, 600), (1024, 768), (1280, 1024), (1440, 960), (1920, 1080))
 
-    def __init__(self, resolution):
-        self.width, self.height = resolution
+    def __init__(self, resolution_id=0):
+        self.width, self.height = self.resolution_list[resolution_id]
         self.screen = set_mode((self.width, self.height))
         # RGB = Red, Green, Blue
         self.screen.fill((0, 0, 0))
