e2612815c42baabf335c8c3560d2ff239dfa5863
rg <rgenius@yandex.ru>
2021-03-17 16:13:30 +0300

    Add right sensor emulation Increase delta

--- Sensor-emulator/main.py
+++ Sensor-emulator/main.py
@@ -3,15 +3,16 @@ import sys
 from time import sleep
 from random import randint
 
-value = 127
-delta = 7
+value_left = 127
+value_right = 127
+delta = 40
 
 while True:
     s = None
 
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket.SOCK_DGRAM)
-        print("Socket successfully created")
+        # print("Socket successfully created")
     except socket.error as err:
         print(f'socket creation failed with error {err}')
 
@@ -42,16 +43,32 @@ while True:
     # connecting to the server
     if s:
         s.connect((host_ip, port))
-        print("the socket has successfully connected to base")
-        print(f'on port == {port} and ip == {host_ip}')
+        # print("the socket has successfully connected to base")
+        # print(f'on port == {port} and ip == {host_ip}')
 
-        if value > 255:
-            value = 255
-        elif value < 0:
-            value = 0
+        value_left += randint(-delta, delta)
 
-        value += randint(-delta, delta)
-        string = f'left {value}\r'
+        if value_left > 255:
+            value_left = 255
+        elif value_left < 0:
+            value_left = 0
+
+        string = 'l' + str(value_left) + '\r'
+        data = bytes(string, 'utf8')  # bytes('Python, bytes', 'utf8')
+        # data = [randint(0, 255)]
+        # data = bytes(data)
+        s.sendall(data)
+        # s.sendto(data, (host_ip, port))
+        print(string)
+
+        value_right += randint(-delta, delta)
+
+        if value_right > 255:
+            value_right = 255
+        elif value_right < 0:
+            value_right = 0
+
+        string = 'r' + str(value_right) + '\r'
         data = bytes(string, 'utf8')  # bytes('Python, bytes', 'utf8')
         # data = [randint(0, 255)]
         # data = bytes(data)
@@ -66,4 +83,4 @@ while True:
 
         s.close()
 
-        sleep(1)
+        sleep(0.5)
